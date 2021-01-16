6d2a661592de8d5ce1b061d850ab0b4fa0937b0d
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-16 16:22:38 +0530

    add properties

--- src/model/Wayback.ts
+++ src/model/Wayback.ts
@@ -5,6 +5,8 @@ const WaybackSchema: Schema = new Schema({
     entityName: { type: String, required: true },
     entityId: { type: String, required: true },
     changes: { type: {}, required: true },
+    old: { type: {}, required: true },
+    new: { type: {}, required: true },
     user: { type: {}, required: true }
 }, { timestamps: true });
 
@@ -12,6 +14,8 @@ interface InterfaceWayback extends Document {
     entityName: string;
     entityId: string;
     changes: unknown;
+    old: unknown;
+    new: unknown;
     user: unknown;
 }
 
