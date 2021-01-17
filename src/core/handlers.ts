10d8f34c4c32fdbd4aa0367d3d5d5f530b6e8e09
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-17 17:20:13 +0530

    delete user after resolving from object

--- src/core/handlers.ts
+++ src/core/handlers.ts
@@ -4,17 +4,18 @@ import { User } from "../interfaces/user";
 import { logit } from "./loggit";
 
 
-function resolveUser(newObject: Document): User {
-    const user = (newObject as HasWayback).__user;
+function resolveUser(newObject: HasWayback): User {
+    const user = (newObject).__user;
     if (!user) {
         throw new Error("unable to get user information");
     }
+    delete newObject.__user;
     return user;
 }
 
 export const handleSave = (newObject: Document, next: (err?: NativeError) => void): void => {
 
-    const user: User = resolveUser(newObject);
+    const user: User = resolveUser(newObject as HasWayback);
 
     (newObject.constructor as Model<Document>).findOne({
         _id: newObject._id
