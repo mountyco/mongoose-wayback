a81ac3f9b99b83f54abe5f5e2a8cf3d284157f7d
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-17 17:17:00 +0530

    resolve user

--- src/core/handlers.ts
+++ src/core/handlers.ts
@@ -1,20 +1,31 @@
-import { Document, Model } from "mongoose";
+import { Document, Model, NativeError } from "mongoose";
+import { HasWayback } from "../interfaces/hasWayback";
+import { User } from "../interfaces/user";
 import { logit } from "./loggit";
 
-export const handleSave = (newObject: Document, next: () => void): void => {
+
+function resolveUser(newObject: Document): User {
+    const user = (newObject as HasWayback).__user;
+    if (!user) {
+        throw new Error("unable to get user information");
+    }
+    return user;
+}
+
+export const handleSave = (newObject: Document, next: (err?: NativeError) => void): void => {
+
+    const user: User = resolveUser(newObject);
 
     (newObject.constructor as Model<Document>).findOne({
         _id: newObject._id
     }).then((oldObject: Document, err) => {
         if (!err) {
-            logit((newObject.constructor as Model<Document>).collection.name, "update", oldObject, newObject)
-                .then(next)
-                .catch(next);
+            logit((newObject.constructor as Model<Document>).collection.name, "update", oldObject, newObject, user)
+                .then(() => next())
+                .catch((err) => next(err));
         } else {
             next();
         }
     });
 
 };
-
-
