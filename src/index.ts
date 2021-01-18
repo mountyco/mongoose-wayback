f1db1ffd72208259172e32efc645306e430fe8a7
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-18 15:51:19 +0530

    update xports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import WaybackPlugin from "./plugin";
 export { HasWayback } from "./interfaces/hasWayback";
+export { Wayback } from "./model/wayback";
+export { logit } from "./core/logit";
 export { WaybackPlugin };
 export default WaybackPlugin;
