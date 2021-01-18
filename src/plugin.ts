541964cc5465fe881a34710f26eb1137ad20300c
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-18 15:45:23 +0530

    use async and await

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,38 @@
-import { Document, Schema } from "mongoose";
-import { handleSave } from "./core/handlers";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Document, Query, Schema } from "mongoose";
+import { handleSave, handleUpdate } from "./core/handlers";
 
 
 const WaybackPlugin = (schema: Schema): void => {
 
-    schema.pre<Document>("save", function (next) {
+    schema.pre<Document>("save", async function () {
         if (this.isNew) {
-            next();
-            return;
+            return true;
         }
-        handleSave(this, next);
+        await handleSave(this);
     });
+
+
+    schema.pre<Query<any, any>>("update", async function () {
+        await handleUpdate(this);
+    });
+
+    schema.pre<Query<any, any>>("updateOne", async function () {
+        await handleUpdate(this);
+    });
+
+    schema.pre<Query<any, any>>("findOneAndUpdate", async function () {
+        await handleUpdate(this);
+    });
+
+    schema.pre<Query<any, any>>("updateMany", async function () {
+        await handleUpdate(this);
+    });
+
+    schema.pre<Query<any, any>>("replaceOne", async function () {
+        await handleUpdate(this);
+    });
+
 };
 
 
